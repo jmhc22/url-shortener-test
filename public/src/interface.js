6a4f89d45c64b3f6f3ab43641f5431dd078bb223
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-12-04 21:53:17 +0000

    jQuery listens for submit, POSTs input to api then receives expected response

--- public/src/interface.js
+++ public/src/interface.js
@@ -1,2 +1,14 @@
 $(document).ready(() => {
+
+  $('#submit-url').click(() => {
+    userInput = $('#input-url').val()
+    postData(userInput)
+  })
+
+  function postData (input) {
+    $.post('/', `{ "url": "${input}" }`, (data) => {
+      response = JSON.parse(data)
+      console.log(response)
+    })
+  }
 })
