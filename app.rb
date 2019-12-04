0b4b51124269efd408f7885c490ab14b8c413090
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-12-04 20:45:42 +0000

    Adds endpoint that receives url in json body and responds with short and orignal url

--- app.rb
+++ app.rb
@@ -4,7 +4,13 @@ require './lib/url_shortener'
 class UrlShortenerApp < Sinatra::Base
 
   before do
-    @url_s = UrlShortener.instance
+    @u_shortener = UrlShortener.instance
+  end
+
+  post '/' do
+    url = JSON.parse(request.body.read)
+    short_url = @u_shortener.create_new(url['url'])
+    { short_url: "/#{short_url}", url: @u_shortener.saved_urls[short_url] }.to_json
   end
 
   run! if app_file == $0
