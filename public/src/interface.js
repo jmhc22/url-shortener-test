6f10d06b16b69404350722811cae5ea305c64671
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-12-04 22:00:39 +0000

    Passes first integration test, created short urls display on page without refresh

--- public/src/interface.js
+++ public/src/interface.js
@@ -1,14 +1,21 @@
 $(document).ready(() => {
+  let listAnchors = []
 
   $('#submit-url').click(() => {
     userInput = $('#input-url').val()
     postData(userInput)
+    $('#input-url').val('http://')
   })
 
   function postData (input) {
     $.post('/', `{ "url": "${input}" }`, (data) => {
-      response = JSON.parse(data)
-      console.log(response)
+      res = JSON.parse(data)
+      listAnchors.push(`<a href=${res.url}>${res.short_url} -> ${res.url}</a>`)
+      updateList()
     })
+  }
+
+  function updateList () {
+    $('#url-list').html(listAnchors.join('\n<br>\n'))
   }
 })
