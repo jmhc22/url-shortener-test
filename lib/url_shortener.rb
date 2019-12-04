649985c6c2842a9ccc171f57895559b44b3a6f6c
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-12-04 20:15:10 +0000

    Refactors - extracts a verify url method from the create_new method

--- lib/url_shortener.rb
+++ lib/url_shortener.rb
@@ -6,20 +6,23 @@ class UrlShortener
 
   def create_new(url)
     surl = extract_short(url)
-    url = 'http://' + url if !url.include? 'http'
-    @saved_urls[surl] = url
+    @saved_urls[surl] = verify(url)
     return surl
   end
 
   private
 
   def extract_short(url)
-    url.sub('http://', '').split('.').select {
-      |u| u.length > 3
+    return url.sub('http://', '').split('.').select { |u|
+      u.length > 3
     }.join.tr('aeiou', '')[0..2] + short_url_nums
   end
 
   def short_url_nums
-    Array.new(3) { rand(9).to_s }.join
+    return Array.new(3) { rand(9).to_s }.join
+  end
+
+  def verify(url)
+    return url.include?('http') ? url : 'http://' + url
   end
 end
