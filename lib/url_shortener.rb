263148683c84aa9e37b0710671b41e7fb947b26b
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-12-04 19:55:01 +0000

    Refactors - extracts private methods from create_new

--- lib/url_shortener.rb
+++ lib/url_shortener.rb
@@ -5,8 +5,18 @@ class UrlShortener
   end
 
   def create_new(url)
-    surl = url.split('.')[1].tr('aeiou', '')[0..2] + Array.new(3) { rand(9).to_s }.join
+    surl = extract_short(url)
     @saved_urls[surl] = url
     return surl
+  end
+
+  private
+
+  def extract_short(url)
+    url.split('.')[1].tr('aeiou', '')[0..2] + short_url_nums
+  end
+
+  def short_url_nums
+    Array.new(3) { rand(9).to_s }.join
   end
 end
